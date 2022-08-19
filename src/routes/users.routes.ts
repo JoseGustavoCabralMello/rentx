9010a9d92379381265ce4729aa87b847eb09f717
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2022-08-19 09:51:43 -0300

    Criando controller de usuário

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+
+const usersRoutes = Router();
+
+const createUSerController = new CreateUserController();
+
+usersRoutes.post("/", createUSerController.handle);
+
+export { usersRoutes };
