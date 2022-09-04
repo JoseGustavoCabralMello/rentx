d347e91856073d3e4a76d4e82018ac3c8e0ceb0a
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2022-09-03 23:05:00 -0300

    Upload Avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
-const createUSerController = new CreateUserController();
+const upload = multer(uploadConfig.upload("./tmp/avatar"));
 
-usersRoutes.post("/", createUSerController.handle);
+const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
+usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticate,
+  upload.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
